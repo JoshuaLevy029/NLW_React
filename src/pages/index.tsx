b548152e3db5886b904005a0bacd67b9f964bfc0
Joshua Levy <51962618+JoshuaLevy029@users.noreply.github.com>
2021-03-21 21:22:32 -0300

    nlw4

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { ExperienceBar } from '../components/ExperienceBar/index';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  )
+}
